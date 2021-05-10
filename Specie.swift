781cf537e786f333772bf2d5304fafe11168147d
Victor Callejas <vcallejas@mail.sfsu.edu>
2021-05-09 19:35:09 -0700

    put it in the wrong folder

---
+++ Specie.swift
@@ -0,0 +1,15 @@
+
+
+import Foundation
+
+class Specie {
+    var name : String
+    var image : String
+    var description : String
+    
+    init(specieName: String, specieImage: String, specieDescription: String){
+        name = specieName
+        image = specieImage
+        description = specieDescription
+    }
+}
