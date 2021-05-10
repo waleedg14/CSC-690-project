68f188799bda8c8e888409798836046f1512066f
Victor Callejas <vcallejas@mail.sfsu.edu>
2021-05-09 19:27:29 -0700

    tabbar file

---
+++ wildify/controller/tabbar.swift
@@ -0,0 +1,20 @@
+//
+//  tabbar.swift
+//  Wildify
+//
+//  Created by Victor Callejas on 4/05/21
+//
+//
+
+import UIKit
+
+class tabbar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
